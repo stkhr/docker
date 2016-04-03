ef4708e8968e24d79cdac4acff4094fb5c3cab9b
stkhr <tshinichi0813@gmail.com>
2016-04-03 21:35:58 +0900

    docker log driver対応

--- build_wordpress/run.sh
+++ build_wordpress/run.sh
@@ -4,5 +4,19 @@
 . ./version.txt
 
 #docker run
-docker run -itd --name mysql01 --expose 3306 stkhr/mysql:ver${mysql_ver}
-docker run -itd -p 8080:80 --link mysql01:db --name wordpress stkhr/wordpress:ver${wordpress_ver}
+docker run -itd -m 512m \
+           --log-driver=fluentd \
+           --log-opt=fluentd-address=localhost:24224 \
+           --log-opt=fluentd-tag=docker.{{.Name}} \
+           --name mysql01 \
+           --expose 3306 \
+           stkhr/mysql:ver${mysql_ver}
+
+docker run -itd -m 256m \
+           -p 8080:80 \
+           --link mysql01:db \
+           --log-driver=fluentd \
+           --log-opt=fluentd-address=localhost:24224 \
+           --log-opt=fluentd-tag=docker.{{.Name}} \
+           --name wordpress \
+           stkhr/wordpress:ver${wordpress_ver}
