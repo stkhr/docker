86ef095c57ab3341801cec5b680dc28186efe71b
stkhr <tshinichi0813@gmail.com>
2016-04-04 21:44:59 +0900

    Dockerfile update

--- build_wordpress/run.sh
+++ build_wordpress/run.sh
@@ -3,11 +3,29 @@
 # version
 . ./version.txt
 
-#docker run
+##docker run for docker log driver
+#docker run -itd -m 512m \
+#           --log-driver=fluentd \
+#           --log-opt=fluentd-address=localhost:24224 \
+#           --log-opt=fluentd-tag=docker.{{.Name}} \
+#           --name mysql01 \
+#           --expose 3306 \
+#           stkhr/mysql:ver${mysql_ver}
+
+#docker run -itd -m 256m \
+#           -p 8080:80 \
+#           --link mysql01:db \
+#           --log-driver=fluentd \
+#           --log-opt=fluentd-address=localhost:24224 \
+#           --log-opt=fluentd-tag=docker.{{.Name}} \
+#           --name wordpress \
+#           stkhr/wordpress:ver${wordpress_ver}
+
+##docker run
+
 docker run -itd -m 512m \
-           --log-driver=fluentd \
-           --log-opt=fluentd-address=localhost:24224 \
-           --log-opt=fluentd-tag=docker.{{.Name}} \
+           -h mysql01 \
+           -v /var/log/docker/mysql01:/var/log \
            --name mysql01 \
            --expose 3306 \
            stkhr/mysql:ver${mysql_ver}
@@ -15,8 +33,8 @@ docker run -itd -m 512m \
 docker run -itd -m 256m \
            -p 8080:80 \
            --link mysql01:db \
-           --log-driver=fluentd \
-           --log-opt=fluentd-address=localhost:24224 \
-           --log-opt=fluentd-tag=docker.{{.Name}} \
+           -h nginx01 \
+           -v /var/log/docker/wordpress:/var/log \
            --name wordpress \
            stkhr/wordpress:ver${wordpress_ver}
+
